e024eaad074c90d8c316d6765c54dc96c79d15e6
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-05-31 16:42:47 +1000

    test middleware

--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -6,6 +6,7 @@ const User = require('mongoose').model('User');
  */
 module.exports = (req, res, next) => {
   if (!req.headers.authorization) {
+    console.log("error 1");
     return res.status(401).end();
   }
 
@@ -15,13 +16,17 @@ module.exports = (req, res, next) => {
   // decode the token using a secret key-phrase
   return jwt.verify(token, process.env.GREEN_YOGA_JWT, (err, decoded) => {
     // the 401 code is for unauthorized status
-    if (err) { return res.status(401).end(); }
+    if (err) {
+      console.log("error 2");
+      return res.status(401).end();
+    }
 
     const userId = decoded.sub;
 
     // check if a user exists
     return User.findById(userId, (userErr, user) => {
       if (userErr || !user) {
+        console.log("error 3");
         return res.status(401).end();
       }
       // pass user details onto next route
