4b19ff2c69fb75e9026d22490122a60994ba2018
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-06-01 11:44:11 +1000

    add attributes to user schema

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -26,9 +26,9 @@ function validateSignupForm(payload) {
     errors.password = 'Password must have at least 8 characters.';
   }
 
-  if (!payload || typeof payload.name !== 'string' || payload.name.trim().length === 0) {
+  if (!payload || typeof payload.firstName !== 'string' || payload.firstName.trim().length === 0) {
     isFormValid = false;
-    errors.name = 'Please provide your name.';
+    errors.firstName = 'Please provide your first name.';
   }
 
   if (!isFormValid) {
