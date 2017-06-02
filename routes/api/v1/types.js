b428518ab6092587856c4759cdc1d1c3f692a04b
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-06-03 09:43:21 +1000

    edit API endpoint for class types

--- routes/api/v1/types.js
+++ routes/api/v1/types.js
@@ -2,8 +2,13 @@ const typeController = require('../../../controllers/typeController')
 const express = require('express');
 const router = new express.Router();
 
+const app = express();
+
+// middleware to prevent access to administrator area
+const administratorCheck = require('../../../middleware/administrator-check');
+
 router.get('/types', typeController.getTypes);
 
-router.post('/types', typeController.postTypes);
+router.post('/types', administratorCheck, typeController.postTypes);
 
 module.exports = router;
