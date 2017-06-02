4b628471326d1b336f6b6ee7b79005e5f4eef37a
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-06-02 11:09:18 +1000

    fix API call reponse for admin and user dashboard

--- routes/api/v1/admin.js
+++ routes/api/v1/admin.js
@@ -4,7 +4,7 @@ const router = new express.Router();
 
 router.get('/dashboard', (req, res) => {
   res.status(200).json({
-    message: "You're authorized to see this secret message.",
+    message: "You're an authorized administrator.",
     // user values passed through from auth middleware
     user: req.user
   });
