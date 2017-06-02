d32b18740bc8d909e193aa55fa25aee0fff35e00
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-06-02 12:34:25 +1000

    add teacher routes

--- routes/api/v1/teacher.js
+++ routes/api/v1/teacher.js
@@ -4,7 +4,7 @@ const router = new express.Router();
 
 router.get('/dashboard', (req, res) => {
   res.status(200).json({
-    message: "You're an authorized administrator.",
+    message: "You're an authorized teacher.",
     // user values passed through from auth middleware
     user: req.user
   });
