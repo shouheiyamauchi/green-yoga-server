7418d6ad6357e1acdf619d1a40eded1aafbdc29f
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-06-05 08:40:48 +1000

    update API end point for user registration

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -117,41 +117,44 @@ function validateLoginForm(payload) {
 }
 
 router.post('/signup', (req, res, next) => {
-  const validationResult = validateSignupForm(req.body);
-  console.log("final run")
-  if (!validationResult.success) {
-    return res.status(400).json({
-      success: false,
-      message: validationResult.message,
-      errors: validationResult.errors
-    });
-  }
-
-  return passport.authenticate('local-signup', (err) => {
-    if (err) {
-      if (err.name === 'MongoError' && err.code === 11000) {
-        // the 11000 Mongo code is for a duplication email error
-        // the 409 HTTP status code is for conflict error
-        return res.status(409).json({
-          success: false,
-          message: 'Check the form for errors.',
-          errors: {
-            email: 'This email is already taken.'
-          }
-        });
-      }
-
+  validateSignupForm(req.body)
+  .then(validationResult => {
+    console.log("final run")
+    if (!validationResult.success) {
       return res.status(400).json({
         success: false,
-        message: 'Could not process the form.'
+        message: validationResult.message,
+        errors: validationResult.errors
       });
     }
 
-    return res.status(200).json({
-      success: true,
-      message: 'You have successfully signed up! Now you should be able to log in.'
-    });
-  })(req, res, next);
+    return passport.authenticate('local-signup', (err) => {
+      if (err) {
+        if (err.name === 'MongoError' && err.code === 11000) {
+          // the 11000 Mongo code is for a duplication email error
+          // the 409 HTTP status code is for conflict error
+          return res.status(409).json({
+            success: false,
+            message: 'Check the form for errors.',
+            errors: {
+              email: 'This email is already taken.'
+            }
+          });
+        }
+
+        return res.status(400).json({
+          success: false,
+          message: 'Could not process the form.'
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: 'You have successfully signed up! Now you should be able to log in.'
+      });
+    })(req, res, next);
+  })
+
 });
 
 router.post('/login', (req, res, next) => {
