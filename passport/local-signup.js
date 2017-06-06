2ebc93c6c6c22b8785cc7a8fc7ed5ee614c988e0
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-06-06 15:54:31 +1000

    update signup controller for date

--- passport/local-signup.js
+++ passport/local-signup.js
@@ -1,4 +1,5 @@
 const User = require('mongoose').model('User');
+const moment = require('moment');
 const PassportLocalStrategy = require('passport-local').Strategy;
 
 /**
@@ -10,13 +11,15 @@ module.exports = new PassportLocalStrategy({
   session: false,
   passReqToCallback: true
 }, (req, email, password, done) => {
+  // make sure the date formatting is universal
+  const dateFormat = "DD/MM/YYYY";
   const userData = {
     email: email.trim(),
     password: password.trim(),
     role: req.body.role.trim(),
     firstName: req.body.firstName.trim(),
     lastName: req.body.lastName.trim(),
-    dob: req.body.dob.trim(),
+    dob: moment(req.body.dob.trim(), dateFormat).format("DD/MM/YYYY"),
     line1: req.body.line1.trim(),
     line2: req.body.line2.trim(),
     suburb: req.body.suburb.trim(),
